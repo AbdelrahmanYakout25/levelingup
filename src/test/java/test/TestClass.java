b7ee38a2502836bf538d6e57507f653b49d620a4
AbdelrahmanYakout25 <abdelrahmanyakout@outlook.com>
2025-08-24 16:39:17 +0300

    Edit TC1

--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
@@ -15,16 +16,18 @@ public class TestClass {
     @Test
     public void tc1()
     {
+        SoftAssert softAssert = new SoftAssert();
 //        WebDriver driver = new ChromeDriver();
         driver.get("https://duckduckgo.com/");
         String actualResult = driver.getTitle();
-        String expectedResult = "Google";
-        assertTrue(actualResult.contains(expectedResult));
+//        String expectedResult = "Google";
+        softAssert.assertTrue(actualResult.contains("Google"), "URL does not contain duckduckgo");
     }
     @AfterTest
     public void tearDown() {
         // will always run even if assertion fails
         driver.quit();
+        System.out.println("test continues here...");
     }
 
     @Test
